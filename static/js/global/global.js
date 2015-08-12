98da51bbc23ce81956edaf48de29393ace3579fb
Wang Yongshuo <yongshuowang@gmail.com>
2015-08-12 07:35:35 -0400

    Add the language buttons draggable

--- static/js/global/global.js
+++ static/js/global/global.js
@@ -26,3 +26,19 @@ function change_language(language){
         }
     });
 }
+
+$(function() {
+    $('#languageWrapper').on('mousedown', 'div', function() {
+        $(this).addClass('draggable').parents().on('mousemove', function(e) {
+            $('.draggable').offset({
+                top: e.pageY - $('.draggable').outerHeight() / 2,
+                left: e.pageX - $('.draggable').outerWidth() / 2
+            }).on('mouseup', function() {
+                $(this).removeClass('draggable');
+            });
+        });
+        e.preventDefault();
+    }).on('mouseup', function() {
+        $('.draggable').removeClass('draggable');
+    });
+});
